1e6fbfbe8b931c008cfa3fe05f65212f2a846d52
Dazboy2020 <darrendowlut@tutanota.com>
2023-04-27 13:02:29 +0200

    erros

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SECONDS = 2.5;
+export const API_KEY = '3d7ae97e-7449-41df-8c9f-86e7b2cce859';
