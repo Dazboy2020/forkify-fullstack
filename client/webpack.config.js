d7c311e0495fcd549f43a9399545488f42ce76f3
Dazboy2020 <darrendowlut@tutanota.com>
2023-04-27 21:12:56 +0200

    changed folder from public to src

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	},
 	devServer: {
 		static: {
-			directory: path.resolve(__dirname, '../public/'),
+			directory: path.resolve(__dirname, 'src'),
 		},
 		port: 3000,
 		open: true,
