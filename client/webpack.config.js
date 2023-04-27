64222166de586467af827146bc8fbebcf8ad1c2d
Dazboy2020 <darrendowlut@tutanota.com>
2023-04-27 10:22:10 +0200

    changed output folder from src to public

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	mode: 'development',
 	entry: './src/js/controller.js',
 	output: {
-		path: path.resolve(__dirname, 'src'),
+		path: path.resolve(__dirname, '../public/'),
 		filename: 'bundle.js',
 	},
 	devServer: {
