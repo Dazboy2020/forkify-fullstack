7a56db3c15432363c634b934b135b38345a0492c
Dazboy2020 <darrendowlut@tutanota.com>
2023-04-28 09:47:10 +0200

    fixed spinner svg icons

--- client/src/js/views/View.js
+++ client/src/js/views/View.js
@@ -51,7 +51,7 @@ export default class View {
 		const markup = `
     <div class="spinner">
             <svg>
-              <use href="./../../img/icons.svg#icon-loader"></use>
+              <use href="icons.svg#icon-loader"></use>
             </svg>
           </div>
     `;
@@ -65,7 +65,7 @@ export default class View {
       <div class="error">
       <div>
         <svg>
-          <use href="./../../img/icons.svg#icon-alert-triangle"></use>
+          <use href="icons.svg#icon-alert-triangle"></use>
         </svg>
       </div>
       <p>${message}</p>
@@ -79,7 +79,7 @@ export default class View {
       <div class="message">
       <div>
         <svg>
-          <use href="./../../img/icons.svg#icon-smile"></use>
+          <use href="icons.svg#icon-smile"></use>
         </svg>
       </div>
       <p>${message}</p>
